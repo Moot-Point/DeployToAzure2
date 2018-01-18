588df1348b94776ce2f576cb86b986da0a572805
Moot-Point <33201393+Moot-Point@users.noreply.github.com>
2018-01-18 10:17:53 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+var http = require("http");
+http.createServer(function (request, response) {
+
+	// send the http header. statys: 200 = ok
+	// content tyoe: text/plain
+	response.writeHead(200, {'Content-Type': 'text/plain'});
+
+	// send the response body as "hello world"
+	response.end('Hello world\n');
+}).listen(8081);
+
+// console will print the message
+console.log('Server running at http://127.0.0.1:8081/');
